20f0c7c4ff002fcd6b1e8959f5c5b601e7459383
Henrique Kuwai <rikezenho@gmail.com>
2018-06-18 18:15:44 -0300

    Silva family wrapper added

---
+++ exs-react/ex/silvaFamily.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Member from './member'
+
+export default props => (
+  <div>
+    <Member name='João' />
+    <Member name='Maria' />
+    <Member name='José' />
+    <Member name='Pedro' />
+  </div>
+)
