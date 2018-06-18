8aec3a62de150e4b650a2274f35da8d11adef916
Henrique Kuwai <rikezenho@gmail.com>
2018-06-18 19:09:05 -0300

    Now ClassComponent has methods and interacts with state

--- exs-react/ex/classComponent.jsx
+++ exs-react/ex/classComponent.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
 
 export default class ClassComponent extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { value: props.initialValue }
+  }
+  sum(delta) {
+    this.setState({ value: this.state.value + delta })
+  }
   render() {
     return (
-      <h1>{this.props.value}</h1>
+      <div>
+        <h1>{this.props.label}</h1>
+        <button onClick={() => this.sum(-1)}>-</button>
+          <div style={{ display: 'inline-block', width: '80px', textAlign: 'center' }}>{this.state.value}</div>
+        <button onClick={() => this.sum(1)}>+</button>
+      </div>
     )
   }
 }
