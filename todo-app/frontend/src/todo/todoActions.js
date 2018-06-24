66c916a9fd5d7dc2ca6ab0c187e8457b0ee78f07
Henrique Kuwai <eu@henriquekuwai.com.br>
2018-06-24 01:32:25 -0300

    Evolving search method and clear methods

--- todo-app/frontend/src/todo/todoActions.js
+++ todo-app/frontend/src/todo/todoActions.js
@@ -8,10 +8,15 @@ export const changeDescription = e => ({
 })
 
 export const search = () => {
-  const request = axios.get(`${URL}?sort=-createdAt`)
-  return {
-    type: 'TODO_SEARCHED',
-    payload: request
+  return (dispatch, getState) => {
+    const description = getState().todo.description
+    const search = description ? `&description__regex=/${description}/gi` : ''
+
+    axios.get(`${URL}?sort=-createdAt${search}`)
+      .then(resp => dispatch({
+        type: 'TODO_SEARCHED',
+        payload: resp.data
+      }))
   }
 }
 
@@ -49,5 +54,8 @@ export const remove = todo => {
 }
 
 export const clear = () => {
-  return { type: 'TODO_CLEAR' }
+  return [
+    { type: 'TODO_CLEAR' },
+    search()
+  ]
 }
