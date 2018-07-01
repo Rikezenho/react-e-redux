2b32ec42ab293ffadc408dbd28ad881c019b4144
Henrique Kuwai <eu@henriquekuwai.com.br>
2018-07-01 02:57:53 -0300

    Finished back-end CRUD with mongoose error messages translate

--- my-money-app/backend/src/config/database.js
+++ my-money-app/backend/src/config/database.js
@@ -2,3 +2,8 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 module.exports = mongoose.connect('mongodb://mymoneyapp:study102030@ds018268.mlab.com:18268/my-money-app')
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
+mongoose.Error.messages.String.enum = "O '{VALUE}' não é válido para o atributo '{PATH}'."
