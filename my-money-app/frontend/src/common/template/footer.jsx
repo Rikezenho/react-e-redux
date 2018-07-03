467e105e1ad4061fc6824b777b2f8a2fb5e7343e
Henrique Kuwai <eu@henriquekuwai.com.br>
2018-07-03 00:03:49 -0300

    Footer added to App

---
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default props => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy; 2018 -{" "}
+      <a href="http://www.henriquekuwai.com.br" target="_blank">
+        Henrique Kuwai
+      </a>
+    </strong>
+  </footer>
+);
