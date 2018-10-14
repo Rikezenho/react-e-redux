185322a4f1ed75ef7e48e8daf39f65f62564985b
Henrique Kuwai <eu@henriquekuwai.com.br>
2018-10-13 23:41:02 -0300

    Adding parent + children component

--- react-16-4/exercicios/src/index.js
+++ react-16-4/exercicios/src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
-import Saudacao from "./components/saudacao";
+
+import Pai from "./components/pai";
+import Filho from "./components/filho";
 
 render(
-  <Saudacao tipo="Bom dia" nome="João" />,
+  <div>
+    <Pai nome="João" sobrenome="Silva">
+      <Filho nome="Pedro" />
+      <Filho nome="Paulo" />
+      <Filho nome="Carla" />
+    </Pai>
+  </div>,
   document.getElementById("root")
 );
